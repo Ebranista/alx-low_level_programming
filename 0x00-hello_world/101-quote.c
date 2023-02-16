92f92e12c5d909dd52772504db8bac718e358284
ebranista <ebranista19@gmail.com>
2023-02-16 13:08:24 -0800

    some piece of are is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -15,9 +15,9 @@
 	int main(void)
 	{
 		write(2,
-			"and that piece of are is useful\" - Dora Korpar, 2015-10-19\n",
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 			59);
-		return(1);
+		return (1);
 	}
 
 
