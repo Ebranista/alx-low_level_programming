b6c9bda288e5408729d0d3c75d9cd3fcf80e7f9d
ebranista <ebranista19@gmail.com>
2023-02-17 11:59:29 -0800

    1 file added

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,7 +18,7 @@ int main(void)
 {
 	int n;
 
-	srand(0);
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
@@ -28,7 +28,5 @@ int main(void)
 	else
 		printf("%d is zero\n", n);
 	return (0);
-
-
 }
 
