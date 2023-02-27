6d43d685a7d8525e9be70e8395e70e399fd2c383
ebranista <ebranista19@gmail.com>
2023-02-27 12:34:17 -0800

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - printes the reversed string
+ * @s - string to be reversed
+ * Return: void
+ */
+
+void print_rev(chaar *s)
+{
+int index = 0;
+while (s[index])
+	index++;
+while (index--)
+{
+	_putchar(s[index]);
+}
+_putchar('\n');
+}
