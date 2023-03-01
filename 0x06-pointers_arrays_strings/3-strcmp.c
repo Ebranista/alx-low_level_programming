fd065ca5ac161c3abf5e7ac63a96e8410d6d19b4
ebranista <ebranista19@gmail.com>
2023-03-01 07:47:14 -0800

    committing message

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - fucntion that compares two strings
+ * @s1: string 1 to compare
+ * @s2: string 2 to campare
+ * Return:  0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
