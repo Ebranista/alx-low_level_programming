7a0b07dfc8a033890ea1b35e0c9b3b99fd866172
= <=>
2023-05-18 11:49:10 -0700

    first commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- * print_dlistint - this function prints all elements
- * of distinct list
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
  * @h: head of the list
- * Return: number of nodes
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
@@ -24,5 +25,6 @@ size_t print_dlistint(const dlistint_t *h)
 		count++;
 		h = h->next;
 	}
+
 	return (count);
 }
